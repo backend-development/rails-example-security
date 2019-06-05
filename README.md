6cafed6468af32defe4fc7927d59feed423d85de
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2019-06-05 14:03:06 +0200

    added seed data, added CSP example, removed passwords and admin status from users/show

--- README.md
+++ README.md
@@ -32,7 +32,8 @@ See https://backend-development.github.io/security.html#slide-6
 ## 3. Authentication
 
 This app does not encrypt passwords and does not
-check if passwords are weak.
+check if passwords are weak.  But let's ignore the encryption
+of the passwords for now.
 
 Write an integration test that tries to 
 create a user with password '12345678'.
@@ -45,5 +46,42 @@ they are stored.
 Now all tests should be green.
 
 See https://backend-development.github.io/security.html#slide-13
+
+### 4. Cross Site Scripting and Content Security Policy
+
+This app is vunerable to XSS.  Let's not fix this by
+sanitizing the input and/or escaping the output, but let's
+use a new method: Create a CSP for your app.  Try out different versions.
+
+But fist: 
+
+1. insert some article with a `<script>` tag in the description.
+2. find the inline style (flex) and inline script (hover over circular button) in `/articles`
+
+
+WARNING: After changing the file `config/initializers/content_security_policy.rb`
+you need to restart the webserver!
+
+
+Questions:
+
+1. Version:
+
+which settings are need to get `bin/webpack-dev-server` to work?
+
+
+2. Version:
+
+try to forbid all `unsafe-inline` script. 
+* which setting do you need? 
+* does the XSS still work?  
+* how about the inline style and inline script?
+
+
+3. Version:
+
+try to use `nonce` to get the (good) inline script to work again.
+
+which settings to you need in rails?
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+one = User.create(
+  name: 'Andrea',
+  email: 'andrea@somewhere.at',
+  password: '12345678',
+  admin: false,
+  homepage: 'https://elsewhere.at'
+)
+
+
+two = User.create(
+  name: 'Brigitte',
+  email: 'brigitte@somewhere.at',
+  password: 'geheim!',
+  admin: true,
+  homepage: 'https://somewhere.at'
+)
+
+three = User.create(
+  name: 'Chris',
+  email: 'chris@somewhere.at',
+  password: 'bierbierbier',
+  admin: false,
+  homepage: 'https://bier.at'
+)
+
+one.articles.create(
+  title: 'HTTP Strict-Transport-Security',
+  description: '<p>The HTTP Strict-Transport-Security response header (often abbreviated as HSTS) lets a web site tell browsers that it should only be accessed using HTTPS, instead of using HTTP.</p>'
+)
+
+two.articles.create(
+  title: 'Cross-Origin Resource Sharing',
+  description: '<p>Cross-Origin Resource Sharing (CORS) is a mechanism that uses additional HTTP headers to tell a browser to let a web application running at one origin (domain) have permission to access selected resources from a server at a different origin. A web application executes a cross-origin HTTP request when it requests a resource that has a different origin (domain, protocol, and port) than its own origin.</p><p>An example of a cross-origin request: The frontend JavaScript code for a web application served from http://domain-a.com uses XMLHttpRequest to make a request for http://api.domain-b.com/data.json.</p><p>For security reasons, browsers restrict cross-origin HTTP requests initiated from within scripts. For example, XMLHttpRequest and the Fetch API follow the same-origin policy. This means that a web application using those APIs can only request HTTP resources from the same origin the application was loaded from, unless the response from the other origin includes the right CORS headers.</p>'
+)
+
+three.articles.create(
+  title: 'Content-Security-Policy',
+  description: '<p>The HTTP Content-Security-Policy response header allows web site administrators to control resources the user agent is allowed to load for a given page. With a few exceptions, policies mostly involve specifying server origins and script endpoints. This helps guard against cross-site scripting attacks (XSS).</p>'
+)
+
